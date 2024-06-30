bc1f9060aa2c614995ab38aeca39af525028bf1b
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-30 18:51:05 +0200

    digtrssing

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ searchBtn.addEventListener("click", searchProfile);
 function searchProfile(){
     console.log("namaste");
 }
+
+let api = "https://api.github.com/user/";
+
+let fetch = document.createElement("script");
+fetch.src = `https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.0/axios.min.js`;
+
+fetch.integrity =   `ha512-DZqqY3PiOvTP9HkjWg06ouCbq+dxqWoJZ/Q+zPYNHmlnI2dQnbJ5bxAHpAMw+LXRm4D72EIRXzvcHQtE8/VQ ==`;
