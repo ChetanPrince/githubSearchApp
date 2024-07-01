1259e15e369417ccaf58e7a59453d233530fd003
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-01 23:42:48 +0200

    a few guidelines are established

--- script2.js
+++ script2.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+
+
+// procedure to follow to display profile of a user to outputArea
+
+
+
+// step 1: user tries to enter a name or an id then clicks button to display the result
+// step 1.1: when user tries to enter a name or id the script converts the name into a string and converts it into lowercase letters 
+// step 1.2: input is then is asyned and awaited until api outputs a result if input name or id is a valid name and id the result should then display profile image in a div and in another div profile stats for each stat interpolation and correspoinding variable s are displayed
