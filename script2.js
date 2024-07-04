d333b2eac4b39786c172733cbc1355899e7d0a85
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-04 08:46:27 +0200

    done with github searchApp

--- script2.js
+++ script2.js
@@ -1,31 +1,30 @@
 async function getGitHubProfile(){
-const username = document.getElementById("name-input").value;
-const profileContainer = document.getElementById("insideOutputBox");
-profileContainer.innerHTML = "";
-if(username === ""){
-    alert("Please enter a Username");
-    return;
-}
-try{
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    if(!response.ok){
-        throw new Error("User Not Found");
+    const username = document.getElementById("name-input").value;
+    const profileContainer = document.getElementById("insideOutputBox");
+    profileContainer.innerHTML = "";
+    if(username === ""){
+        alert("Please enter a username");
+        return;
     }
-    const data = await response.json();
-    displayProfile(data);
-}
-catch(error){
-    profileContainer.innerHTML = `<p>${error.message}</p>`;
-}
-
+    try{
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        if(!response.ok){
+            throw new Error("UserName Not Found");
+        }
+        displayProfile(data);
+    }
+    catch(error){
+        profileContainer.innerHTML = `<p>${error.message}</p>`;
+    }
 }
 
 // function displayProfile(data) where profile container is set with innerhtml interpolation method and image in a profile div with other data like h2 name or login oaragraohs username bio repos follows followign location and html url for profile on github are set inside another div inside the main div with class naem profile detailsthen divs are ended
 
 function displayProfile(data){
     const profileContainer = document.getElementById("insideOutputBox");
-    profileContainer.innerHTML = `<div id="profile"><img src="${data.avatar_url}" alt="${data.login}">
-    <div id="profile-details">
+    profileContainer.innerHTML = `<div class="profile"><img src="${data.avatar_url}" alt="${data.login}">
+    <div class="profile-details">
     <h2>${data.name || data.login}</h2>
     <p><strong>Username:</strong>${data.login}</p>
     <p><strong>Bio:</strong>${data.bio|| "N/A"}</p>
